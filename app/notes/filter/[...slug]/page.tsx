954de25d9ee2e0d80308a92ceb443bde3cb59cff
your_nameAnhelina <bogdanangelina@gmail.com>
2025-09-01 15:54:46 +0300

    fixed layout

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -4,18 +4,24 @@ import { QueryClient, dehydrate } from "@tanstack/react-query";
 import { HydrationBoundary } from "@tanstack/react-query";
 import { fetchNotes } from "@/lib/api";
 
-export default async function NotesPage() {
+interface NotesPageProps {
+  params: Promise<{ slug: string[] }>;
+}
+
+export default async function NotesPage({ params }: NotesPageProps) {
+  const { slug } = await params;
+    const tag = slug?.[0] ?? "";
   const queryClient = new QueryClient();
 
   await queryClient.prefetchQuery({
-    queryKey: ["notes", "", 1],
-    queryFn: () => fetchNotes(1, ""),
+    queryKey: ["notes", tag, 1],
+    queryFn: () => fetchNotes(1, tag),
   });
 
   return (
     <div className={css.app}>
       <HydrationBoundary state={dehydrate(queryClient)}>
-        <NoteListClient />
+        <NoteListClient tag={tag}/>
       </HydrationBoundary>
     </div>
   );
